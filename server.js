6f11417795ccfe8cb8c87c9a8571b8240526e93e
stopod <gjkv8@icloud.com>
2023-10-01 16:45:53 +0900

    edit port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 10000;
 const authRoute = require("./routers/auth");
 const postsRoute = require("./routers/posts");
 const userRoute = require("./routers/user");
